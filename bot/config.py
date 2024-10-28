99b4b2efe24846082894dff758633c2a57f3deb0
Ravindu12123 <140928846+Ravindu12123@users.noreply.github.com>
2024-10-28 09:32:51 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -2,19 +2,19 @@ import os
 
 class Config:
 
-    BOT_TOKEN = ""
+    BOT_TOKEN = "7879947375:AAFwuV4am9rCXBUxoHX5BVuxewm2TcDXrUA"
 
-    SESSION_NAME = ""
+    SESSION_NAME = "ytubbot"
 
-    API_ID = ""
+    API_ID = "28234847"
 
-    API_HASH = ""
+    API_HASH = "a494667c22f483263f1e7612f4f1a576"
 
-    CLIENT_ID = ""
+    CLIENT_ID = "113211858679-63hr0vcqb9528d3779vibkv6hdofthq8.apps.googleusercontent.com"
 
-    CLIENT_SECRET = ""
+    CLIENT_SECRET = "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"
 
-    AUTH_USERS = [942731625]
+    AUTH_USERS = [1387186514]
 
     VIDEO_DESCRIPTION = ""
 
